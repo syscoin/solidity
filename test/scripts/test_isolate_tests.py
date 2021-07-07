98712c261b2748e5985301387aaf0116d7925a27
Marenz <github@supradigital.org>
2021-07-07 13:34:33 +0200

    Use more readable and maintainable multiline strings in py test script

--- test/scripts/test_isolate_tests.py
+++ test/scripts/test_isolate_tests.py
@@ -9,54 +9,61 @@ from unittest_helpers import FIXTURE_DIR, load_fixture
 from isolate_tests import extract_docs_cases
 # pragma pylint: enable=import-error
 
+import textwrap
 
 CODE_BLOCK_RST_PATH = FIXTURE_DIR / 'code_block.rst'
 CODE_BLOCK_RST_CONTENT = load_fixture(CODE_BLOCK_RST_PATH)
 CODE_BLOCK_WITH_DIRECTIVES_RST_PATH = FIXTURE_DIR / 'code_block_with_directives.rst'
 CODE_BLOCK_WITH_DIRECTIVES_RST_CONTENT = load_fixture(CODE_BLOCK_WITH_DIRECTIVES_RST_PATH)
 
+# Formats code to contain only one indentation and terminate with a \n
+def formatCase(text):
+    return textwrap.indent(textwrap.dedent(text.lstrip("\n")), "    ") + "\n"
 
 class TestExtractDocsCases(unittest.TestCase):
     def setUp(self):
         self.maxDiff = 10000
 
-    def test_solidity_block(self):
-        expected_cases = [
-            "    // SPDX-License-Identifier: GPL-3.0\n"
-            "    pragma solidity >=0.7.0 <0.9.0;\n"
-            "\n"
-            "    contract C {\n"
-            "        function foo() public view {}\n"
-            "    }\n"
-            "\n"
-            "\n",
 
-            "    contract C {}\n"
-            "\n",
-        ]
+    def test_solidity_block(self):
+        expected_cases = [formatCase(case) for case in [
+            """
+                // SPDX-License-Identifier: GPL-3.0
+                pragma solidity >=0.7.0 <0.9.0;
+
+                contract C {
+                    function foo() public view {}
+                }
+
+            """,
+            """
+                contract C {}
+            """,
+        ]]
 
         self.assertEqual(extract_docs_cases(CODE_BLOCK_RST_PATH), expected_cases)
 
     def test_solidity_block_with_directives(self):
-        expected_cases = [
-            "    // SPDX-License-Identifier: GPL-3.0\n"
-            "    pragma solidity >=0.7.0 <0.9.0;\n"
-            "\n"
-            "    contract C {\n"
-            "        function foo() public view {}\n"
-            "    }\n"
-            "\n"
-            "\n",
+        expected_cases = [formatCase(case) for case in [
+            """
+                // SPDX-License-Identifier: GPL-3.0
+                pragma solidity >=0.7.0 <0.9.0;
 
-            "    contract C {}\n"
-            "\n",
+                contract C {
+                    function foo() public view {}
+                }
 
-            "    contract D {}\n"
-            "    :linenos:\n"
-            "\n",
-
-            "    contract E {}\n"
-            "\n",
-        ]
+            """,
+            """
+                contract C {}
+            """,
+            """
+                contract D {}
+                :linenos:
+            """,
+            """
+                contract E {}
+            """,
+        ]]
 
         self.assertEqual(extract_docs_cases(CODE_BLOCK_WITH_DIRECTIVES_RST_PATH), expected_cases)
