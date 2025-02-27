d7073bada9705a998b9d48874d9c01fbfd165f26
Matheus Aguiar <matheus.pit@gmail.com>
2025-02-27 19:23:53 -0300

    Update and add tests

--- test/libsolidity/syntaxTests/storageLayoutSpecifier/layout_specified_by_last_ancestor_contract.sol
+++ test/libsolidity/syntaxTests/storageLayoutSpecifier/layout_specified_by_last_ancestor_contract.sol
@@ -3,4 +3,4 @@ contract B is A {}
 contract C is B layout at 42 {}
 contract D is C {}
 // ----
-// TypeError 8894: (65-83): Storage layout can only be specified in the most derived contract.
+// TypeError 8894: (79-80): Cannot inherit from a contract with a custom storage layout.
